a80717a02f32be7cc2f1d53f7b3cab5987b525bf
ITomatoes <113779815+ITomatoes@users.noreply.github.com>
2023-01-15 12:48:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var firstname = "Yogith";
+console.log(firstname);
+var lastname = "Perumal";
+console.log(lastname);
+var fullname = firstname.concat(lastname);
+var fullname2 = "Full name is ";
+var fullname3 = fullname2.concat(fullname)
+console.log(fullname3)
+var length = "Length of my full name is "
+var length2 = fullname3.length;
+var length3 = length.concat(length2);
+console.log(length3);
+console.log("Do you know what's 77 + 33?");
+console.log("It's not 100.");
+var a = 77;
+var b = 33;
+var add = a + b;
+var H = "It's "
+var E = H.concat(add)
+console.log(E)
+
